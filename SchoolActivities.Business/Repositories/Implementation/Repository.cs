376d9d6ecdb3ee5b23090db63b2d2f5039a3dd41
peshopbs2 <peshopbs2@gmail.com>
2026-03-19 00:13:09 -0800

    add repository

---
+++ SchoolActivities.Business/Repositories/Implementation/Repository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolActivities.Business.Repositories.Interfaces;
+using SchoolActivities.Data.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Business.Repositories.Implementation
+{
+    public class Repository<T> : IRepository<T>
+           where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(e => EF.Property<Guid>(e, "Id") == id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FilterAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return await query.Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<T> Query()
+        {
+            return _dbSet.AsQueryable();
+        }
+    }
+}
---
+++ SchoolActivities.Business/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Business.Repositories.Interfaces
+{
+    /// <summary>
+    /// Defines a generic repository interface for common data access operations
+    /// </summary>
+    /// <typeparam name="T">The type of the entity</typeparam>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Retrives single entity by its GUID
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity</param>
+        /// <param name="includes">Optional navigation properties to include</param>
+        /// <returns>The entity if found; otherwise, null</returns>
+        Task<T?> GetByIdAsync(Guid id, params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Retrieves all entities
+        /// </summary>
+        /// <param name="includes">Optional navigation properties to include</param>
+        /// <returns>A list of all entities</returns>
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Finds all entities matching the specified predicate
+        /// </summary>
+        /// <param name="predicate">The filtering condition</param>
+        /// <param name="includes">Optional navigation properties to include</param>
+        /// <returns>A list of matching entities</returns>
+        Task<IEnumerable<T>> FilterAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Add a new entity to the repository
+        /// </summary>
+        /// <param name="entity">The entity to add</param>
+        Task AddAsync(T entity);
+
+        /// <summary>
+        /// Updates an existing entity
+        /// </summary>
+        /// <param name="entity">The entity to update</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Removes an entity from the repository
+        /// </summary>
+        /// <param name="entity">The entity to remove</param>
+        void Remove(T entity);
+
+        /// <summary>
+        /// Commits all changes made in the current unit of work
+        /// </summary>
+        /// <returns>The number of affected rows</returns>
+        Task<int> CommitAsync();
+
+        /// <summary>
+        /// Provides a queryable interface for the repository, allowing for LINQ queries
+        /// </summary>
+        /// <returns>An <see cref="IQueryable{T}"/> for entity type.</returns>
+        public IQueryable<T> Query();
+    }
+}
