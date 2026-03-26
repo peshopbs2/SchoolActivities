81b6eab5f5a0f10abe70ab03f4d4c129500cb42a
peshopbs2 <peshopbs2@gmail.com>
2026-03-26 00:28:00 -0800

    add activity dtos

---
+++ SchoolActivities.Models/Dtos/Activity/ActivityResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Models.Dtos.Activity
+{
+    public class ActivityResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
