f001091404f1c66c20afd2f1e296a8177eb3807d
peshopbs2 <peshopbs2@gmail.com>
2026-03-26 00:10:59 -0800

    add student dtos

---
+++ SchoolActivities.Models/Dtos/Student/StudentRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Models.Dtos.Student
+{
+    public class StudentRequestDto
+    {
+        public Guid? StudentId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
