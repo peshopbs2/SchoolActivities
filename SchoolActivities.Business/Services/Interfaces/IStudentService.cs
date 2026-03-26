b4ee3bb89455a2c22448601a6880eb3b19e3a2f0
peshopbs2 <peshopbs2@gmail.com>
2026-03-26 00:11:12 -0800

    add student service interface

---
+++ SchoolActivities.Business/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,18 @@
+﻿using SchoolActivities.Models.Dtos.Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Business.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<StudentResponseDto>> GetAllAsync();
+        Task<StudentResponseDto> GetByIdAsync(Guid studentId);
+        Task<StudentResponseDto> CreateAsync(StudentRequestDto dto);
+        Task<StudentResponseDto?> UpdateAsync(Guid studentId, StudentRequestDto dto);
+        Task DeleteAsync(Guid studentId);
+    }
+}
