96c8a9a5a8bd71a86a8f422742e6d9c28f2d6b3f
peshopbs2 <peshopbs2@gmail.com>
2026-03-26 00:28:06 -0800

    add enrollment dtos

---
+++ SchoolActivities.Models/Dtos/Enrollment/EnrollmentRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Models.Dtos.Enrollment
+{
+    public class EnrollmentRequestDto
+    {
+        public Guid StudentId { get; set; }
+        public Guid ActivityId { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ SchoolActivities.Models/Dtos/Enrollment/EnrollmentResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolActivities.Models.Dtos.Enrollment
+{
+    public class EnrollmentResponseDto
+    {
+        public Guid StudentId { get; set; }
+        public string StudentFirstName { get; set; } = string.Empty;
+        public string StudentLastName { get; set; } = string.Empty;
+
+        public Guid ActivityId { get; set; }
+        public string ActivityName { get; set; } = string.Empty;
+
+        public DateTime EnrolledAtUtc { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
